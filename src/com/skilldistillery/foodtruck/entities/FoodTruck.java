276a15882c8309e51f25c77bd7dc565c95f94d36
Lawrence Leppert <lxleppert@gmail.com>
2022-06-24 16:08:12 -0600

    Initial commit

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	/*
+	 * unique numeric id, a name ("TacoRific", "Mediterranean Medic", etc.), food
+	 * type ("Tacos", "Falafel", etc.), and a numeric rating
+	 */
+}
