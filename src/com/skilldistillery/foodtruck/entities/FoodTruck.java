4091eabd70f182f56a449be153cae01922432846
Lawrence Leppert <lxleppert@gmail.com>
2022-06-28 12:02:18 -0600

    Format readability test #3

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,54 +2,55 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 
-	private int truckId;
-	private int truckRating;
-	private String truckName;
-	private String truckFoodType;
-	private static int nextTruckId;
+    private int truckId;
+    private int truckRating;
+    private String truckName;
+    private String truckFoodType;
+    private static int nextTruckId;
 
-	static {
-		nextTruckId = ((int) (Math.random() * 1000)) * 100;
-	}
+    static {
+	nextTruckId = ((int) (Math.random() * 1000)) * 100;
+    }
 
-	public FoodTruck() {
-		this.truckId = ++nextTruckId;
-	}
+    public FoodTruck() {
+	this.truckId = ++nextTruckId;
+    }
 
-	public FoodTruck(String truckName, String truckFoodType, int truckRating) {
-		this();
-		this.truckName = truckName;
-		this.truckFoodType = truckFoodType;
-		this.truckRating = truckRating;
-	}
+    public FoodTruck(String truckName, String truckFoodType, int truckRating) {
+	this();
+	this.truckName = truckName;
+	this.truckFoodType = truckFoodType;
+	this.truckRating = truckRating;
+    }
 
-	public String getTruckName() {
-		return truckName;
-	}
+    public String getTruckName() {
+	return truckName;
+    }
 
-	public void setTruckName(String truckName) {
-		this.truckName = truckName;
-	}
+    public void setTruckName(String truckName) {
+	this.truckName = truckName;
+    }
 
-	public String getTruckFoodType() {
-		return truckFoodType;
-	}
+    public String getTruckFoodType() {
+	return truckFoodType;
+    }
 
-	public void setTruckFoodType(String truckFoodType) {
-		this.truckFoodType = truckFoodType;
-	}
+    public void setTruckFoodType(String truckFoodType) {
+	this.truckFoodType = truckFoodType;
+    }
 
-	public int getTruckRating() {
-		return truckRating;
-	}
+    public int getTruckRating() {
+	return truckRating;
+    }
 
-	public void setTruckRating(int truckRating) {
-		this.truckRating = truckRating;
-	}
+    public void setTruckRating(int truckRating) {
+	this.truckRating = truckRating;
+    }
 
-	@Override
-	public String toString() {
-		return "Food Truck [" + truckId + "], Truck Name: [" + truckName + "], Food Type: [" + truckFoodType
-				+ "], Truck Rating: [" + truckRating + "]";
-	}
+    @Override
+    public String toString() {
+	return "Food Truck [" + truckId + "], Truck Name: [" + truckName
+		+ "], Food Type: [" + truckFoodType + "], Truck Rating: ["
+		+ truckRating + "]";
+    }
 }
